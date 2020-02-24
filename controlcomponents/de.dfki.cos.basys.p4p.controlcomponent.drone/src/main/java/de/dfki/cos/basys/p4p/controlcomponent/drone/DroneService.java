1337cac0dcf9abd002b27afe04661af94ec46693
Andreas Luxenburger <andreas.luxenburger@dfki.de>
2020-02-24 17:41:49 +0100

    specified basic drone interface

--- controlcomponents/de.dfki.cos.basys.p4p.controlcomponent.drone/src/main/java/de/dfki/cos/basys/p4p/controlcomponent/drone/DroneService.java
+++ controlcomponents/de.dfki.cos.basys.p4p.controlcomponent.drone/src/main/java/de/dfki/cos/basys/p4p/controlcomponent/drone/DroneService.java
@@ -1,5 +1,15 @@
 package de.dfki.cos.basys.p4p.controlcomponent.drone;
 
-public interface DroneService {
+import java.util.List;
 
+public interface DroneService {
+	void moveToSymbolicPosition(String position);
+	void takeOff();
+	void startVideoStream();
+	void stopVideoStream();
+	void land();
+	String getMissionState();
+	String getStatus();
+	List<String> getSymbolicPositions();
+	void reset();
 }

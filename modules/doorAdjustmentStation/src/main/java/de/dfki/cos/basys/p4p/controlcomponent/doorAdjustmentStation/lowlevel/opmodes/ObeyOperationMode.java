5e0af7108d0dc65ef4aef45cf4967be3cf9ccdc0
Jonas Mohr <jonas.mohr@dfki.de>
2025-09-10 15:16:12 +0200

    262 - Call reset in opModes

--- modules/doorAdjustmentStation/src/main/java/de/dfki/cos/basys/p4p/controlcomponent/doorAdjustmentStation/lowlevel/opmodes/ObeyOperationMode.java
+++ modules/doorAdjustmentStation/src/main/java/de/dfki/cos/basys/p4p/controlcomponent/doorAdjustmentStation/lowlevel/opmodes/ObeyOperationMode.java
@@ -44,4 +44,10 @@ public class ObeyOperationMode extends BaseDoorAdjustmentStationOperationMode {
         DoorAdjustmentStationServiceImpl.currentOpMode = OPMode.NONE;
         DoorAdjustmentStationServiceImpl.currentTask = TASK.NONE;
     }
+
+    @Override
+    public void onResetting() {
+        getService(DoorAdjustmentStationService.class).reset();
+        executing = false;
+    }
 }
--- modules/doorAdjustmentStation/src/main/java/de/dfki/cos/basys/p4p/controlcomponent/doorAdjustmentStation/lowlevel/opmodes/ShowOperationMode.java
+++ modules/doorAdjustmentStation/src/main/java/de/dfki/cos/basys/p4p/controlcomponent/doorAdjustmentStation/lowlevel/opmodes/ShowOperationMode.java
@@ -45,4 +45,10 @@ public class ShowOperationMode extends BaseDoorAdjustmentStationOperationMode {
         DoorAdjustmentStationServiceImpl.currentOpMode = OPMode.NONE;
         DoorAdjustmentStationServiceImpl.currentTask = TASK.NONE;
     }
+
+    @Override
+    public void onResetting() {
+        getService(DoorAdjustmentStationService.class).reset();
+        executing = false;
+    }
 }

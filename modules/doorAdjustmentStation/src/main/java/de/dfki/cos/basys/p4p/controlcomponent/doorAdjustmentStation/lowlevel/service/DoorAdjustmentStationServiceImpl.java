52828c482b0e845fdc1ccacec33eed39a71648ed
Jonas Mohr <jonas.mohr@dfki.de>
2025-09-10 13:53:16 +0200

    262 - Reset instructions

--- modules/doorAdjustmentStation/src/main/java/de/dfki/cos/basys/p4p/controlcomponent/doorAdjustmentStation/lowlevel/service/DoorAdjustmentStationServiceImpl.java
+++ modules/doorAdjustmentStation/src/main/java/de/dfki/cos/basys/p4p/controlcomponent/doorAdjustmentStation/lowlevel/service/DoorAdjustmentStationServiceImpl.java
@@ -149,7 +149,7 @@ public class DoorAdjustmentStationServiceImpl implements DoorAdjustmentStationSe
 
     @Override
     public void reset() {
-
+        publish("/aiquama/showInstruction/reset", "");
     }
 
     @Override
